0e8df91e3604e49210bb352d8ddb230bffa6b6dd
prashant <tomar.prashant162@gmail.com>
2017-07-21 14:30:27 +0530

    First Push

---
+++ Assets/Scripts/Blast.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blast : MonoBehaviour {
+    public float lifeTime;
+    public float flameLifeTime;
+    public GameObject blastObject;
+    private GameObject bomb;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(WaitAndDestroy(lifeTime));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    IEnumerator WaitAndDestroy(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
+        blastObject.SetActive(true);
+        gameObject.GetComponent<AudioSource>().Play();
+        //yield return new WaitForSeconds(flameLifeTime);
+        //Destroy(blastObject);
+    }
+}
